8745589dd83846ed5628c2ae4b921d8d445a163d
zach1502 <zach1502@hotmail.com>
2024-05-18 23:33:30 -0700

    maintain consistent cookie

--- server/src/index.js
+++ server/src/index.js
@@ -20,17 +20,12 @@ app.use(cookieParser());
 app.use(express.json({ limit: '512mb' }));
 
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	next();
-});
-
 app.use(cors({
-	origin: '*',
-	credentials: true
-}));
+	origin: 'http://localhost:3000', // set the allowed origin to your client's address
+	credentials: true, // allow credentials
+	methods: ['GET', 'POST', 'PUT', 'DELETE'], // allowed methods
+	allowedHeaders: ['Content-Type'] // allowed headers
+  }));
 
 app.get('/', async (req, res) => {
 	const uuid = getUUIDFromCookie(req);
@@ -55,7 +50,7 @@ app.listen(PORT, () => {
 
 app.post('/chat', async (req, res) => {
 	const uuid = getUUIDFromCookie(req);
-	console.log(req.cookies);
+	console.log(uuid);
 	const { image, audio } = req.body; // Get content from request body
 	try {
 		const base64Audio = audio.replace(/^data:audio\/webm;base64,/, "");
